79e34b79507bb843b400864469908a2e9294f0a4
leah <l0583280864@gmail>
2025-08-04 02:29:19 +0300

    Initial commit

---
+++ AiImageApi/Data/Models/DrawingCommand.cs
@@ -0,0 +1,22 @@
+﻿namespace AiImageApi.Data.Models
+{
+    public class DrawingCommand
+    {
+        public string? Id { get; set; } = string.Empty;
+        public string? Type { get; set; }
+        public double? X { get; set; }
+        public double? Y { get; set; }
+        public double? Radius { get; set; }
+        public double? Width { get; set; }
+        public double? Height { get; set; }
+        public string? Fill { get; set; }
+
+        public double? X1 { get; set; }
+        public double? Y1 { get; set; }
+        public double? X2 { get; set; }
+        public double? Y2 { get; set; }
+        public string? Stroke { get; set; }
+        public double? StrokeWidth { get; set; }
+        public string? Name { get; set; }
+    }
+}
